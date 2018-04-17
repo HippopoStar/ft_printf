c3e315dcb9d5f6164364f672f984242feaa83968
Leo CABANES <lcabanes@student.42.fr>
2018-04-17 03:20:15 +0200

    Test de "ft_printf"

--- update.sh
+++ update.sh
@@ -7,10 +7,14 @@ cp ~/Documents/15_04_2018/projet_ft_printf/libftprintf/Makefile ./
 cp -r ~/Documents/15_04_2018/projet_ft_printf/libftprintf/includes ./
 cp -r ~/Documents/15_04_2018/projet_ft_printf/libftprintf/srcs_libft ./
 cp -r ~/Documents/15_04_2018/projet_ft_printf/libftprintf/srcs_libftprintf ./
+@echo '\033[36m_____\033[00m'
 git status
 git add *
+@echo '\033[36m_____\033[00m'
 git status
 git commit -m "Test de \"ft_printf\""
+@echo '\033[36m_____\033[00m'
 git status
 git push
+@echo '\033[36m_____\033[00m'
 git status
