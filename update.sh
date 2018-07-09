580cdf7fa85a964ddd8ec2696a16592c1eeec2f7
Leo CABANES <lcabanes@student.42.fr>
2018-07-09 10:33:39 +0200

    Modification "update.sh"

--- update.sh
+++ update.sh
@@ -3,10 +3,11 @@ git rm Makefile
 git rm -r ./includes
 git rm -r ./srcs_libft
 git rm -r ./srcs_libftprintf
-cp ~/Documents/15_04_2018/projet_ft_printf/libftprintf/Makefile ./
-cp -r ~/Documents/15_04_2018/projet_ft_printf/libftprintf/includes ./
-cp -r ~/Documents/15_04_2018/projet_ft_printf/libftprintf/srcs_libft ./
-cp -r ~/Documents/15_04_2018/projet_ft_printf/libftprintf/srcs_libftprintf ./
+git clone https://github.com/HippopoStar/branche_algo_et_IA.git /tmp/
+cp /tmp/branche_algo_et_IA/projet_ft_printf/libftprintf/Makefile ./
+cp -r /tmp/branche_algo_et_IA/projet_ft_printf/libftprintf/includes ./
+cp -r /tmp/branche_algo_et_IA/projet_ft_printf/libftprintf/srcs_libft ./
+cp -r /tmp/branche_algo_et_IA/projet_ft_printf/libftprintf/srcs_libftprintf ./
 echo '\033[36m_____\033[00m'
 git status
 git add *
